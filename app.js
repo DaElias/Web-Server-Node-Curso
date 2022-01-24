115e46fbe1ed503db19863cb22b5520b038224cb
DaElias <58747265+DaElias@users.noreply.github.com>
2022-01-24 12:31:35 -0500

    new app in public

--- app.js
+++ app.js
@@ -12,12 +12,18 @@ app.get("/", (req, res) => {
 
 });
 
-app.get("/generic", (req, res) => {
-    res.sendFile(__filename+'/public/generic.html');
-});
+app.get("*", (req, res) => {
+    //   res.render('home');
+        res.sendFile(__filename+'/public/index.html');
+    
+    });
 
-app.get("/elements", (req, res) => {
-    res.sendFile(__filename+'/public/elements.html');
-});
+// app.get("/generic", (req, res) => {
+//     res.sendFile(__filename+'/public/generic.html');
+// });
+
+// app.get("/elements", (req, res) => {
+//     res.sendFile(__filename+'/public/elements.html');
+// });
 
 app.listen(port, () => console.log(`Running => http://localhost:${port}`));
