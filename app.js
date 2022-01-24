002cf1ad669e49088c092f78014ec38c5a375cb0
DaElias <58747265+DaElias@users.noreply.github.com>
2022-01-24 11:58:00 -0500

    web sever

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+// app.set('view engine', 'hbs');
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+//   res.render('home');
+    res.sendFile(__filename+'/public/index.html');
+
+});
+
+app.get("/generic", (req, res) => {
+    res.sendFile(__filename+'/public/generic.html');
+});
+
+app.get("/elements", (req, res) => {
+    res.sendFile(__filename+'/public/elements.html');
+});
+
+app.listen(port, () => console.log(`Running => http://localhost:${port}`));
